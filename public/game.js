6e6accea8150398e0cfa9feb85155013a3e06c73
Chris Bilson <cbilson@pobox.com>
2013-11-04 17:13:48 -0800

    bail out early if we are paused - reduce nesting

--- public/game.js
+++ public/game.js
@@ -16,39 +16,41 @@ function update() {
     var ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (!paused) {
-        x = x + speedx;
-        if (x>canvas.width-35) {
-            speedx=-5;
-            x=canvas.width-35
-        }
+    if (paused)
+        return;
 
-        else if (x<0) {
-            speedx=5;
-            x=0;
-        }
-        else if (x<mousex){
-            speedx=5;
-        }
-        else if (x>mousex){
-            speedx=-5;
-        }
-        y=y+speedy;
-        if (y>canvas.height-25){
-            speedy=-5;
-            y=canvas.height-25;
-        }
-        else if (y<0){
-            speedy=5;
-            y=0;
-        }
-        else if (y<mousey){
-            speedy=5;
-        }
-        else if (y>mousey){
-            speedy=-5;
-        }
+    x = x + speedx;
+    if (x>canvas.width-35) {
+        speedx=-5;
+        x=canvas.width-35
     }
+
+    else if (x<0) {
+        speedx=5;
+        x=0;
+    }
+    else if (x<mousex){
+        speedx=5;
+    }
+    else if (x>mousex){
+        speedx=-5;
+    }
+    y=y+speedy;
+    if (y>canvas.height-25){
+        speedy=-5;
+        y=canvas.height-25;
+    }
+    else if (y<0){
+        speedy=5;
+        y=0;
+    }
+    else if (y<mousey){
+        speedy=5;
+    }
+    else if (y>mousey){
+        speedy=-5;
+    }
+
     ctx.drawImage(img, x, y, 35, 25);
 }
 
