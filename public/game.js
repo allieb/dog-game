e5f398eb0e5cfdcefba35e8a552e750834ae7d71
Chris Bilson <cbilson@pobox.com>
2013-11-04 17:07:57 -0800

    whitespace

--- public/game.js
+++ public/game.js
@@ -1,44 +1,44 @@
- var canvas = document.getElementById('tutorial');
- function update() {
- 	var ctx = canvas.getContext('2d');
+var canvas = document.getElementById('tutorial');
+function update() {
+    var ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-	
-	if (!paused) {
-		x = x + speedx;
-		if (x>canvas.width-35) {
-			speedx=-5;
-			x=canvas.width-35
-		}
-	
-		else if (x<0) {
-			speedx=5;
-			x=0;
-		}
-		else if (x<mousex){
-			speedx=5;
-		}
-		else if (x>mousex){
-			speedx=-5;
-		}
-		y=y+speedy;
-		if (y>canvas.height-25){
-			speedy=-5;
-			y=canvas.height-25;
-		}
-		else if (y<0){
-			speedy=5;
-			y=0;
-		}
-		else if (y<mousey){
-			speedy=5;
-		}
-		else if (y>mousey){
-			speedy=-5;
-		}
-	}
- 	ctx.drawImage(img, x, y, 35, 25);
+
+    if (!paused) {
+        x = x + speedx;
+        if (x>canvas.width-35) {
+            speedx=-5;
+            x=canvas.width-35
+        }
+
+        else if (x<0) {
+            speedx=5;
+            x=0;
+        }
+        else if (x<mousex){
+            speedx=5;
+        }
+        else if (x>mousex){
+            speedx=-5;
+        }
+        y=y+speedy;
+        if (y>canvas.height-25){
+            speedy=-5;
+            y=canvas.height-25;
+        }
+        else if (y<0){
+            speedy=5;
+            y=0;
+        }
+        else if (y<mousey){
+            speedy=5;
+        }
+        else if (y>mousey){
+            speedy=-5;
+        }
+    }
+    ctx.drawImage(img, x, y, 35, 25);
 }
- 
+
 var x = 0;
 var y = 0;
 var speedx = 5;
@@ -49,12 +49,12 @@ var mousex=canvas.width/2;
 var mousey=canvas.height/2;
 img.src = "dog-head-md.png";
 function togglePause() {
-	if (event.keyCode == 32) {
-		paused = !paused;
-	}
+    if (event.keyCode == 32) {
+        paused = !paused;
+    }
 }
 function mouseMoved(){
-console.log (event.clientX);	
+    console.log (event.clientX);
 }
 window.setInterval(update, 100);
 document.onkeypress = togglePause;
